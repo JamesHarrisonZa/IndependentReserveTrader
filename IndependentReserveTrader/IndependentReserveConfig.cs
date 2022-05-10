f3b9f0ee0d46c8d95b8a3d85f21fba123cd3e209
James.Harrison <James.Harrison@Serko.com>
2022-05-10 18:29:46 +1200

    Read config

---
+++ IndependentReserveTrader/IndependentReserveConfig.cs
@@ -0,0 +1,6 @@
+namespace IndependentReserveTrader;
+
+public class IndependentReserveConfig {
+    public string ApiKey { get; init; }
+    public string ApiSecret { get; init; }
+}
--- IndependentReserveTrader/Program.cs
+++ IndependentReserveTrader/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using IndependentReserveTrader;
+
+// Build a config object, using env vars and JSON providers.
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Get values from the config given their key and their target type.
+var independentReserveConfig = config
+    .GetRequiredSection("IndependentReserve")
+    .Get<IndependentReserveConfig>();
+
+Console.WriteLine(independentReserveConfig.ApiKey);
+Console.WriteLine(independentReserveConfig.ApiSecret);
