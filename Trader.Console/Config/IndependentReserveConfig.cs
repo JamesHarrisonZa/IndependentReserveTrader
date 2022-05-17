43d36970199f24324caa919ac9149e13eae68f9b
James.Harrison <James.Harrison@Serko.com>
2022-05-17 17:38:48 +1200

    Delete console project

--- Trader.Console/Config/IndependentReserveConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Trader.Console.Config;
-
-public class IndependentReserveConfig
-{
-  public string BaseUrl { get; init; }
-  public string ApiKey { get; init; }
-  public string ApiSecret { get; init; }
-}
