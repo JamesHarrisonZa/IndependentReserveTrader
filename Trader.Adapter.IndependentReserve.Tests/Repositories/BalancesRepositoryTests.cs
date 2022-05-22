072ade5724b443fed522dd2e3bd48c70ee685c89
James.Harrison <James.Harrison@Serko.com>
2022-05-22 12:39:33 +1200

    WIP RepositoryTests

---
+++ Trader.Adapter.IndependentReserve.Tests/Repositories/BalancesRepositoryTests.cs
@@ -0,0 +1,19 @@
+// namespace Trader.Adapter.IndependentReserve.Tests;
+
+// public class BalancesRepositoryTests
+// {
+//     private readonly Mock<IMarketRepository> _marketRepositoryMock;
+//     private readonly IBalancesRepository _balancesRepository;
+
+//     public BalancesRepositoryTests()
+//     {
+//         _marketRepositoryMock = new Mock<IMarketRepository>();
+//         _balancesRepository = new BalancesRepository(_marketRepositoryMock.Object);
+//     }
+
+//     [Fact]
+//     public void Given_CryptoCurrency_When_GetBalance_Then_Returns_Balance()
+//     {
+
+//     }
+// }
--- Trader.Adapter.IndependentReserve.Tests/Usings.cs
+++ Trader.Adapter.IndependentReserve.Tests/Usings.cs
@@ -1,2 +1,4 @@
 global using Moq;
 global using Xunit;
+global using Trader.Domain.OutboundPorts;
+global using Trader.Adapter.IndependentReserve.Repositories;
