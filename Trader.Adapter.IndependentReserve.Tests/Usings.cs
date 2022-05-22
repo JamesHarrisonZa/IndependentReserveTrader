a6b58d03b938eed2b1a546b83765ea92209fe529
James.Harrison <James.Harrison@Serko.com>
2022-05-22 13:07:39 +1200

    Added AutoFixture

--- Trader.Adapter.IndependentReserve.Tests/Usings.cs
+++ Trader.Adapter.IndependentReserve.Tests/Usings.cs
@@ -1,4 +1,7 @@
 global using Moq;
 global using Xunit;
+global using AutoFixture;
+global using Trader.Domain.Enums;
 global using Trader.Domain.OutboundPorts;
+global using Trader.Adapter.IndependentReserve.Config;
 global using Trader.Adapter.IndependentReserve.Repositories;
