7ac092d66efbb8673c3b1cc795bd84d609ffaee9
James.Harrison <James.Harrison@Serko.com>
2022-05-22 13:07:47 +1200

    More WIP tests

--- Trader.Adapter.IndependentReserve.Tests/Repositories/BalancesRepositoryTests.cs
+++ Trader.Adapter.IndependentReserve.Tests/Repositories/BalancesRepositoryTests.cs
@@ -1,19 +1,30 @@
-// namespace Trader.Adapter.IndependentReserve.Tests;
+namespace Trader.Adapter.IndependentReserve.Tests;
 
-// public class BalancesRepositoryTests
-// {
-//     private readonly Mock<IMarketRepository> _marketRepositoryMock;
-//     private readonly IBalancesRepository _balancesRepository;
+public class BalancesRepositoryTests
+{
+    private readonly Fixture _fixture;
+    private readonly Mock<IMarketRepository> _marketRepositoryMock;
+    private readonly IBalancesRepository _balancesRepository;
 
-//     public BalancesRepositoryTests()
-//     {
-//         _marketRepositoryMock = new Mock<IMarketRepository>();
-//         _balancesRepository = new BalancesRepository(_marketRepositoryMock.Object);
-//     }
+    public BalancesRepositoryTests()
+    {
+        _fixture = new Fixture();
+        var independentReserveConfigFake = _fixture
+            .Build<IndependentReserveConfig>()
+            .With(c => c.BaseUrl, "https://example.com")
+            .Create();
 
-//     [Fact]
-//     public void Given_CryptoCurrency_When_GetBalance_Then_Returns_Balance()
-//     {
+        _marketRepositoryMock = new Mock<IMarketRepository>();
 
-//     }
-// }
+        _balancesRepository = new BalancesRepository(
+            _marketRepositoryMock.Object, 
+            independentReserveConfigFake
+        );
+    }
+
+    [Fact]
+    public void Given_CryptoCurrency_When_GetBalance_Then_Returns_Balance()
+    {
+        var cryptoCurrency = CryptoCurrency.BTC;
+    }
+}
