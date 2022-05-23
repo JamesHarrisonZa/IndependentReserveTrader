d9942fed00269b0308e30173a212718dc5720eb0
James.Harrison <James.Harrison@Serko.com>
2022-05-23 22:08:57 +1200

    Tests for GetBalanceValue

--- Trader.Adapter.IndependentReserve.Tests/Repositories/BalancesRepositoryTests.cs
+++ Trader.Adapter.IndependentReserve.Tests/Repositories/BalancesRepositoryTests.cs
@@ -25,22 +25,53 @@ public class BalancesRepositoryTests
     public async void Given_CryptoCurrency_When_GetBalance_Then_Returns_Balance()
     {
         var cryptoCurrency = CryptoCurrency.BTC;
-        
-        var expected = 0.42m;
 
-        var fakeAccount = _fixture
-            .Build<Account>()
-            .With(a => a.CurrencyCode, CurrencyCode.Xbt)
-            .With(a => a.AvailableBalance, expected)
-            .Create();
-
+        var expectedBalance = 0.42m;
+        var fakeAccount = GetFakeBtcAccount(expectedBalance);
         _clientMock
             .Setup(c => c.GetAccountsAsync())
-            .ReturnsAsync(new List<Account>{ fakeAccount });
+            .ReturnsAsync(new List<Account> { fakeAccount })
+            .Verifiable();
 
-        var actual = await _balancesRepository
+        var actualBalance = await _balancesRepository
             .GetBalance(cryptoCurrency);
 
-        Assert.Equal(expected, actual);
+        Mock.VerifyAll();
+        Assert.Equal(expectedBalance, actualBalance);
+    }
+
+  [Fact]
+    public async void Given_CryptoCurrency_And_FiatCurrency_When_GetBalanceValue_Then_Returns_GetBalanceValue()
+    {
+        var cryptoCurrency = CryptoCurrency.BTC;
+        var fiatCurrency = FiatCurrency.NZD;
+
+        var fakeAccount = GetFakeBtcAccount(0.42m);
+        _clientMock
+            .Setup(c => c.GetAccountsAsync())
+            .ReturnsAsync(new List<Account> { fakeAccount })
+            .Verifiable();
+
+        _marketRepositoryMock
+            .Setup(mr => mr.GetLastPrice(cryptoCurrency, fiatCurrency))
+            .ReturnsAsync(42000m)
+            .Verifiable();
+        
+        var expectedBalanceValue = 17640m;
+
+        var actualBalanceValue = await _balancesRepository
+            .GetBalanceValue(cryptoCurrency, fiatCurrency);
+
+        Mock.VerifyAll();
+        Assert.Equal(expectedBalanceValue, actualBalanceValue);
+    }
+
+    private Account GetFakeBtcAccount(decimal balance)
+    {
+        return _fixture
+            .Build<Account>()
+            .With(a => a.CurrencyCode, CurrencyCode.Xbt)
+            .With(a => a.AvailableBalance, balance)
+            .Create();
     }
 }
